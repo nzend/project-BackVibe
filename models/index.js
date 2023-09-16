4a6bd00cbc381e910d3102fc934445e896b0c9fb
nzend <115723110+nzend@users.noreply.github.com>
2023-09-16 21:10:15 +0300

    fix products get request

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 const User = require("./user")
 const Exercise = require("./exercise")
 const Filter = require("./filter")
+const Product = require("./product")
 
 module.exports = {
   User,
   Exercise,
   Filter,
+  Product
 };
