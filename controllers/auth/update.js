d764023b14192ae3c14f33f69e0d9ebce13c02dc
Oleksandr <bragardlin@gmail.com>
2023-09-16 19:34:21 +0300

    user routes is done

--- controllers/auth/update.js
+++ controllers/auth/update.js
@@ -1,9 +1,43 @@
-const update = async (req, res) => {
-  const body = req.body;
-  console.log('body');
-  const now = new Date(new Date() - 568036800000);
-  console.log(now);
-  res.json(body);
-}
+const calculateCalories = require("../../helpers/calculateCalories");
+const { User } = require("../../models");
 
-module.exports = update
+const update = async (req, res) => {
+  const avatarURL = req.file ? req.file.path : "";
+  const {
+    name,
+    height,
+    currentWeight,
+    desiredWeight,
+    birthday,
+    blood,
+    sex,
+    levelActivity,
+  } = req.body;
+
+  const dailyСalories = calculateCalories(req.body);
+  const dailyTime = 110;
+  const userData = {
+    name,
+    avatarURL,
+    dailyTime,
+    dailyСalories,
+    bodyParameters: {
+      height,
+      currentWeight,
+      desiredWeight,
+      birthday,
+      blood,
+      sex,
+      levelActivity,
+    },
+  };
+
+  const updatedUser = await User.findByIdAndUpdate(req.user._id, userData, {
+    new: true,
+    select: "-_id -createdAt -updatedAt -password -verify -verificationToken",
+  });
+
+  res.json(updatedUser);
+};
+
+module.exports = update;
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,11 +1,11 @@
 const isValidId = require("./isValidId");
 const validateBody = require("./validateBody");
 const authenticate = require("./authenticate");
-
+const upload = require("./uploadAvatar");
 
 module.exports = {
   isValidId,
   validateBody,
   authenticate,
-  
+  upload,
 };
