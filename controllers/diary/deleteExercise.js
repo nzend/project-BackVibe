01f3ee3594180f705d83acd3b09468f19682d514
Anzhela Bazalinska <anzhela.bazalinskaya@gmail.com>
2023-09-18 20:51:13 +0300

    Update deleteExercise.js

--- controllers/diary/deleteExercise.js
+++ controllers/diary/deleteExercise.js
@@ -1,13 +1,25 @@
 const { Diary } = require('../../models');
 const { HttpError } = require('../../helpers');
+const { Exercise } = require('../../models');
+
 
 const deleteExercise = async (req, res) => {
   const { _id: owner } = req.user;
-  const { date, id } = req.body;
+  const { date, exerciseId } = req.body;
+
+  
+  const exercise = await Exercise.findOne(
+    { _id: exerciseId },
+    '-createdAt -updatedAt'
+  );
+
+  const deleteExercise = {
+    _id: exercise._id
+  };
 
   const result = await Diary.findOneAndUpdate(
     { owner, date },
-    { $pull: { exercises: { _id: id } } },
+    { $pull: { exercises: deleteExercise } },
     { new: true }
   );
 
