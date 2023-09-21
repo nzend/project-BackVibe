2dc0d8d0a6ad98d294d6eade7e13948713e9a25d
nzend <115723110+nzend@users.noreply.github.com>
2023-09-21 12:31:35 +0300

    fix delete

--- controllers/diary/deleteExercise.js
+++ controllers/diary/deleteExercise.js
@@ -3,22 +3,38 @@ const { HttpError } = require('../../helpers');
 
 const deleteExercise = async (req, res) => {
   const { _id: owner } = req.user;
-  const { date, exerciseId } = req.body;
+  const { date, exerciseId } = req.query;
 
 
-  const result = await Diary.findOneAndUpdate(
-    { owner, date },
-    { $pull: { exercises: { _id: exerciseId } } }, 
-    { new: true }
-  );
-
-  if (!result) {
-    throw HttpError(404, 'Not Found');
+  if (!date || !exerciseId) {
+    throw HttpError(400, 'Please provide both date and exerciseId');
   }
 
-  res.status(200).json({
-    message: 'Exercise deleted successfully',
-  });
+  const filter = { date, owner };
+
+
+    const diaryEntry = await Diary.findOne(filter);
+
+    if (!diaryEntry) {
+      throw HttpError(404, 'Not Found');
+    }
+    console.log("ENTRY", diaryEntry.exercises);
+    
+    const exercise = diaryEntry.exercises.find(exercise => exercise._id.toString() === exerciseId);
+    console.log(exercise);
+
+    if (!exercise) {
+      throw HttpError(404, 'Exercise not found');
+    }
+
+
+    await Diary.findOneAndUpdate(
+      filter,
+      { $pull: { exercises: { _id: exerciseId } } }
+    );
+
+    res.status(200).json({ message: 'Exercise deleted successfully' });
+
 };
 
 module.exports = deleteExercise;
--- controllers/diary/deleteProduct.js
+++ controllers/diary/deleteProduct.js
@@ -1,26 +1,39 @@
 const { Diary } = require('../../models');
-const { HttpError } = require("../../helpers");
+const { HttpError } = require('../../helpers');
 
 const deleteProduct = async (req, res) => {
-    const { _id: owner } = req.user;
-    const { date, productId } = req.body; 
+  const { _id: owner } = req.user;
+  const { date, productId } = req.query;
 
+  if (!date || !productId) {
+    throw HttpError(400, 'Please provide both date and productId');
+  }
+
+  const filter = { date, owner };
   
-      const result = await Diary.findOneAndUpdate(
-        { owner, date },
-        { $pull: { products: { _id: productId } } }, 
-        { new: true }
-      );
-  
-      if (!result) {
-        throw HttpError(404, "Not Found");
-      }
-  
-      res.status(200).json({
-        message: "Product deleted successfully",
-      });
-   
+    const diaryEntry = await Diary.findOne(filter);
+
+    if (!diaryEntry) {
+      throw HttpError(404, 'Diary entry not found');
+    }
+
+    const product = diaryEntry.products.find(
+      (product) => product._id.toString() === productId
+    );
+
+    if (!product) {
+      throw HttpError(404, 'Product not found');
+    }
+
+
+    await Diary.findOneAndUpdate(
+      filter,
+      { $pull: { products: { _id: product._id } } },
+      { new: true }
+    );
+
+    res.status(200).json({ message: 'Product deleted successfully' });
+ 
 };
 
 module.exports = deleteProduct;
-
