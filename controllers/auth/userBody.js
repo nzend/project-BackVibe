825595f175e70089b4aa72acfb2fde10851ca9f3
nzend <115723110+nzend@users.noreply.github.com>
2023-09-24 21:14:59 +0300

    Update userBody.js

--- controllers/auth/userBody.js
+++ controllers/auth/userBody.js
@@ -2,14 +2,15 @@ const calculateCalories = require("../../helpers/calculateCalories");
 const { User } = require("../../models");
 
 const userBody = async (req, res) => {
-  const { _id } = req.user;
+  const { _id, email, name } = req.user;
+  
   const body = req.body;
   const dailyСalories = calculateCalories(body);
   const dailyTime = 110;
 
   const updatedUser = await User.findByIdAndUpdate(
     _id,
-    { bodyParameters: { ...body }, dailyСalories, dailyTime },
+    { bodyParameters: { ...body }, dailyСalories, dailyTime, email, name },
     {
       new: true,
       select: "-_id -createdAt -updatedAt -password -verify -verificationToken",
