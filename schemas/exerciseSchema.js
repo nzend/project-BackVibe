b2db23cf67fce73bb9216fd084c8bd25e1af7e39
nzend <115723110+nzend@users.noreply.github.com>
2023-09-26 20:11:29 +0300

    add schemas validate

--- schemas/exerciseSchema.js
+++ schemas/exerciseSchema.js
@@ -1,25 +1,13 @@
 const Joi = require('joi');
 
 const exerciseSchema = Joi.object({
-  bodyPart: Joi.string().required().messages({
-    'string.base': 'Body part must be a string',
-    'any.required': 'Body part is a required field',
+  exerciseId: Joi.string().required().messages({
+    'string.base': 'exerciseId part must be a string',
+    'any.required': 'exerciseId part is a required field',
   }),
-  equipment: Joi.string().required().messages({
-    'string.base': 'Equipment must be a string',
-    'any.required': 'Equipment is a required field',
-  }),
-  gifUrl: Joi.string().required().messages({
-    'string.base': 'Gif url must be a string',
-    'any.required': 'Gif url is a required field',
-  }),
-  name: Joi.string().required().messages({
-    'string.base': 'Name must be a string',
-    'any.required': 'Name is a required field',
-  }),
-  target: Joi.string().required().messages({
-    'string.base': 'Target must be a string',
-    'any.required': 'Target is a required field',
+  date: Joi.string().required().messages({
+    'string.base': 'Date must be a string',
+    'any.required': 'Date is a required field',
   }),
   burnedCalories: Joi.number().required().messages({
     'number.base': 'Burned calories must be a number',
--- schemas/productSchema.js
+++ schemas/productSchema.js
@@ -21,3 +21,4 @@ const productSchema = Joi.object({
 });
 
 module.exports = productSchema;
+
